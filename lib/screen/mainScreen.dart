102390857a0cd907ce0b85e3f5d9e194d2ed4295
Hong <39299265+hsk1321@users.noreply.github.com>
2022-03-10 23:50:48 +0900

    loading location test 1

--- lib/screen/mainScreen.dart
+++ lib/screen/mainScreen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:location/location.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class MainScreen extends StatefulWidget {
   @override
@@ -6,8 +9,34 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreen extends State<MainScreen> {
+  final Location location = Location();
+
+  late double w;
+  late double k;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Weather"),
+          actions: [
+            IconButton(
+                onPressed: () {
+                  location.onLocationChanged
+                      .listen((LocationData currentLocation) {
+                    w = currentLocation.altitude!;
+                    k = currentLocation.latitude!;
+                  });
+                },
+                icon: FaIcon(
+                  FontAwesomeIcons.locationArrow,
+                  color: Colors.white,
+                ))
+          ],
+        ),
+        body: Container(
+            child: Center(
+          child: Text(""),
+        )));
   }
 }
